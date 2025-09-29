f477d1b99cbc067c1ed859d9cd9551cacc83a5ea
melomatthewdev <devscustomers@outlook.com>
2025-09-29 10:27:11 -0500

    config Database in deploy

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
-import express from 'express';
-import empleadosRoutes from './routes/empleados.routes.js';
-import indexRoutes from './routes/index.routes.js';
+import app from './app.js';
+import { PORT } from './config.js';
 
-const app = express();
+app.listen(PORT)
+console.log('Server running on port', PORT);
 
-app.use(express.json());
 
-app.use(indexRoutes);
-app.use('/api', empleadosRoutes);
-
-app.use((req, res, next) => {
-    res.status(404).json({
-        message: 'Endpoint no encontrado'
-    })
-})
-
-app.listen(3000)
-console.log('Server running on port 3000');
